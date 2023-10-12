2c5abba167fd28bba375198f89ea6a35e19a0af0
MrCracker-OP <mr.cracker103@gmail.com>
2023-10-12 13:49:13 +0530

    fixed message show in client side

--- server/Server.java
+++ server/Server.java
@@ -33,9 +33,8 @@ public class MultiThreadedServer {
 
     public void broadcastMessage(String message, ClientHandler sender) {
         for (ClientHandler client : clients) {
-            if (client != sender) {
                 client.sendMessage(message);
-            }
+
         }
     }
 
